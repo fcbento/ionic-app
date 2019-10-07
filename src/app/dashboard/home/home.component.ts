d68d03e2cf2ce4e9f5d3bf34b0036f2efe5aae4a
Felipe Costa <felipe.costa@resourceit.com>
2019-10-07 09:35:30 -0300

    folder structure

---
+++ src/app/dashboard/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+
+  public user: any;
+
+  constructor(private menu: MenuController, private auth: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.user = this.auth.getUser();
+  }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  navigate(option: string) {
+    this.router.navigate([`home/${option}`]);
+    this.menu.close();
+  }
+
+}
